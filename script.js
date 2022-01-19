49f3e772ac87902a3b30d6badd76972d2fb3a589
Shahzaad Tayob <shahzaad.tayob@bp.com>
2022-01-19 14:06:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const robot = require("robotjs");
+//Get the mouse position, retuns an object with x and y. 
+
+
+(async function(){
+    await new Promise(resolve => setTimeout(resolve, 20000));
+    let mouse=robot.getMousePos();
+    console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
+    //Move the mouse down by 100 pixels.
+    robot.moveMouse(mouse.x,mouse.y+1);
+    robot.moveMouse(mouse.x,mouse.y-1);
+    console.log("mouse moved");
+    
+})();
